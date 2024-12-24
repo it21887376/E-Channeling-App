3c82e26a3a34327e32c428a10467c98696140de4
M.T.Ayagamage <88220442+it21887376@users.noreply.github.com>
2024-12-24 18:21:37 +0530

    Add files via upload

---
+++ app/src/main/java/com/mad/hospitalmanagementsystem/SelectRegistration.java
@@ -0,0 +1,14 @@
+package com.mad.hospitalmanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_registration);
+    }
+}
